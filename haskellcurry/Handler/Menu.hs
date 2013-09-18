486c7e5f2df85fe13287bd4b31349436ea88d600
Florent Delannoy <florent.delannoy@gmail.com>
2013-09-18 20:12:00 +1200

    initial commit v2

---
+++ haskellcurry/Handler/Menu.hs
@@ -0,0 +1,9 @@
+module Handler.Menu where
+
+import Import
+
+getMenuR :: Handler Html
+getMenuR = error "Not yet implemented: getMenuR"
+
+postMenuR :: Handler Html
+postMenuR = error "Not yet implemented: postMenuR"
