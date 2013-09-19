83c5c44c2c12041427ec4ce291d371a0d7dd8878
Florent Delannoy <florent@catalyst.net.nz>
2013-09-19 13:36:07 +1200

    working!

--- haskellcurry/Handler/Menu.hs
+++ haskellcurry/Handler/Menu.hs
@@ -2,8 +2,26 @@ module Handler.Menu where
 
 import Import
 
+entryForm :: Form CurryOrder
+entryForm = renderDivs $ CurryOrder
+    <$> areq   textField "Curry Type" Nothing
+    <*> areq   textField "Hotness (Mild/Medium/Hot)" Nothing
+    <*> areq   textField "Garlic Naan? (extra dollar)" Nothing
+    <*> areq   textField "Name" Nothing
+
 getMenuR :: Handler Html
-getMenuR = error "Not yet implemented: getMenuR"
+getMenuR = do
+    (curryOrderWidget, enctype) <- generateFormPost entryForm
+    defaultLayout $(widgetFile "menu")
 
 postMenuR :: Handler Html
-postMenuR = error "Not yet implemented: postMenuR"
+postMenuR = do
+    ((res,curryOrderWidget),enctype) <- runFormPost entryForm
+    case res of
+         FormSuccess order -> do
+            orderId <- runDB $ insert order
+            setMessage $ toHtml $ (curryOrderUnderling order) <> " Ordered!"
+            redirect $ MenuR
+         _ -> defaultLayout $ do
+                setMessage "Please fill ALL OF THE FIELDS."
+                redirect $ MenuR
